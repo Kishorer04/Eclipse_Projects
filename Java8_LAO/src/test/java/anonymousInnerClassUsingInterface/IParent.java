c7837228515537d2cf4895d8e8121e80fd79a802
Ravikumar <kravikumar@galaxe.com>
2024-07-15 14:38:51 +0530

    AIC as method arguments

--- Java8_LAO/src/test/java/anonymousInnerClassUsingInterface/IParent.java
+++ Java8_LAO/src/test/java/anonymousInnerClassUsingInterface/IParent.java
@@ -1,7 +1,6 @@
 package anonymousInnerClassUsingInterface;
 
 public interface IParent {
-	
 
-	void findPartner();
+	void findPartner();  // By default the methods inside an interface are public, abstract
 }
